61ef41b73009a02c99e934e631a69255fd913479
RamonSanGabriel <jeshero11@gmail.com>
2025-02-19 14:14:56 +0800

    update

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+// const app = express();
+const { DB_HOST, PORT = 8080 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+    console.log('Database connect successfully');
+  })
+  .catch((error) =>
+    console.log(`Server not running. Error message:${error.message}`)
+  );
