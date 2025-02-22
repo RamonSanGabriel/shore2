2f995439fa2740e192fb4315cc4006b85ad45a1b
RamonSanGabriel <jeshero11@gmail.com>
2025-02-22 16:11:02 +0800

    font size

--- shore2/src/data/main.js
+++ shore2/src/data/main.js
@@ -6,6 +6,6 @@ export const main = {
   icon: 'home',
   route: '/',
   content:
-    'Have you ever dreamed of living your very own island out in the open sea? For all we know, all of us have thought of that luxury. And now, it has become a reality. Because at SMDC Shore 2 Residences at Mall of Asia Complex in Pasay City, itU+2019s as if you own a private island in middle of the bustling city. The pathways thick with trees and shrubbery, the glassy cobalt waters, the views are so breathtaking you wouldn’t believe you’re still in Metro Manila. What’s great is that even in this setting, you’ll still have access to all the conveniences you want, this property being within SM Mall of Asia Complex.',
+    "Have you ever dreamed of living your very own island out in the open sea? For all we know, all of us have thought of that luxury. And now, it has become a reality. Because at SMDC Shore 2 Residences at Mall of Asia Complex in Pasay City, it's as if you own a private island in middle of the bustling city. The pathways thick with trees and shrubbery, the glassy cobalt waters, the views are so breathtaking you wouldn't believe you're still in Metro Manila. What's great is that even in this setting, you'll still have access to all the conveniences you want, this property being within SM Mall of Asia Complex.",
   image: facade,
 };
