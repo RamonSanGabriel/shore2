5f43dd9842f6215d9eeb51445ee9309c682f790d
RamonSanGabriel <jeshero11@gmail.com>
2025-02-24 10:10:43 +0800

    NavBar.jsx created

--- shore2/src/components/Header/Header.jsx
+++ shore2/src/components/Header/Header.jsx
@@ -1,30 +1,11 @@
+import NavBar from '../NavBar/NavBar';
 import css from './Header.module.css';
-import { NavLink } from 'react-router-dom';
-import { navLinks } from '../../data/navLinks';
-import shoreLogo from '../../assets/Shore Residences Logo.avif';
 
 const Header = () => {
-  const { path } = navLinks;
-
   return (
     <>
       <header className={css.header}>
-        <nav className={css.nav}>
-          <NavLink className={css.navLink} to={path}>
-            <img className={css.logo} src={shoreLogo} />
-          </NavLink>
-          <ul className={css.navList}>
-            {navLinks.map(({ id, name, path }) => {
-              return (
-                <li className={css.navListItems} key={id}>
-                  <NavLink className={css.navLink} to={path}>
-                    <p className={css.navListName}>{name}</p>
-                  </NavLink>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+        <NavBar />
       </header>
     </>
   );
---
+++ shore2/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import { navLinks } from '../../data/navLinks';
+import shoreLogo from '../../assets/Shore Residences Logo.avif';
+import css from './NavBar.module.css';
+
+const NavBar = () => {
+  const { path } = navLinks;
+  return (
+    <div>
+      <nav className={css.nav}>
+        <NavLink className={css.navLink} to={path}>
+          <img className={css.logo} src={shoreLogo} />
+        </NavLink>
+        <ul className={css.navList}>
+          {navLinks.map(({ id, name, path }) => {
+            return (
+              <li className={css.navListItems} key={id}>
+                <NavLink className={css.navLink} to={path}>
+                  <p className={css.navListName}>{name}</p>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
